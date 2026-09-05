8af6f6eb3e7abed3da8b37fc7b7ec6455aa6a5d3
CoYildiz <yusufyildizr@gmail.com>
2026-09-06 01:50:42 +0300

    some extras

---
+++ more_content/pizza.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import seaborn as sea
+import matplotlib.pyplot as plt
+
+
+
+sea.set()
+plt.axis([0, 50, 0, 50])                                 # scale axes (0 to 50)
+plt.xticks(fontsize=14)                                  # set x axis ticks
+plt.yticks(fontsize=14)                                  # set y axis ticks
+plt.xlabel("Reservations", fontsize=14)                  # set x axis label
+plt.ylabel("Pizzas", fontsize=14)                        # set y axis label
+X, Y = np.loadtxt("data/pizza.txt", skiprows=1, unpack=True)  # load data
+plt.plot(X, Y, "bo")                                     # plot data
+plt.show()
